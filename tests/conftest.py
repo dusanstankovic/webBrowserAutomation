f50d3e6b6a61f6d594574070e5e262ee7dba9790
Dusan Stankovic <sdusan@gmail.com>
2022-07-11 20:00:37 +0200

    implement pytest browser options

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,29 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome')
 
 
 @pytest.fixture(scope='class')
 def setup(request):
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    browser_name = request.config.getoption('--browser_name')
+    if browser_name == 'chrome':
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
+    elif browser_name == 'firefox':
+        service = FirefoxService(GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=service)
+    elif browser_name == 'edge':
+        service = EdgeService(EdgeChromiumDriverManager().install())
+        driver = webdriver.Edge(service=service)
     driver.get('https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC')
     driver.maximize_window()
     request.cls.driver = driver
