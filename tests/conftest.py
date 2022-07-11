f266370aa8305dbe302f2aea6bb7eb540eb5bc8a
Dusan Stankovic <sdusan@gmail.com>
2022-07-11 18:18:03 +0200

    fix dependencies and test login

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,16 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.service import Service
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope='class')
-def setup():
+def setup(request):
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     driver.get('https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC')
     driver.maximize_window()
+    request.cls.driver = driver
+    yield
+    driver.close()
+
--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -1,9 +1,17 @@
 import pytest
+from selenium.webdriver.common.by import By
 
 
 @pytest.mark.usefixtures('setup')
 class TestOne:
 
-    def test_end2end(self):
-        pass
+    def test_end2end(self, setup):
 
+        username = self.driver.find_element(By.NAME, 'username')
+        username.send_keys('john')
+
+        password = self.driver.find_element(By.NAME, 'password')
+        password.send_keys('demo')
+
+        submit_button = self.driver.find_element(By.XPATH, '//input[@type="submit"]')
+        submit_button.click()
